f81e7a2d612225535e2a9de81cef548bea40e2c8
Jacob Lin <jacoblin@Jacobs-MacBook-Air.local>
2025-05-04 00:20:17 -0400

    programmed login / icon button

--- Frontend/CaloriMeter/src/components/NavBar.tsx
+++ Frontend/CaloriMeter/src/components/NavBar.tsx
@@ -1,11 +1,43 @@
-import { useState } from 'react'
-import {AppBar, Box, Button, IconButton, Menu, MenuItem, Toolbar, Typography} from '@mui/material';
+import { useState } from 'react';
+
+// mui components
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+
+// mui icons 
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
 import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
 
 export default function NavBar() {
     const [loggedIn, setLoggedIn] = useState(true); // toggles the display of the login button
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null); // HTML element that the Menu will be positioned relative to
+
+    function handleLogin() {
+      setLoggedIn(true);
+    }
+
+    function handleLogout() {
+      setLoggedIn(false);
+      handleMenuClose();
+    }
+
+    // open the menu when the profile icon is clicked 
+    function handleMenuOpen(event: React.MouseEvent<HTMLElement>) {
+      setAnchorEl(event.currentTarget);
+    }
+
+    // close the menu 
+    function handleMenuClose() {
+      setAnchorEl(null);
+    }
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -24,35 +56,39 @@ export default function NavBar() {
                         CaloriMeter
                     </Typography>
 
-                    {   !loggedIn ?
-                        <Button color="inherit">Login</Button> :  
+                    { loggedIn ?
                         <div>
-                        <IconButton
-                          size="large"
-                          aria-label="account of current user"
-                          aria-controls="menu-appbar"
-                          aria-haspopup="true"
-                          color="inherit"
-                        >
-                          <AccountCircle />
-                        </IconButton>
-                        <Menu
-                          id="menu-appbar"
-                          anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                          }}
-                          keepMounted
-                          transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                          }}
-                          open={false}
-                        >
+                          <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            color="inherit"
+                            onClick={handleMenuOpen}
+                          >
+                            <AccountCircle />
+                          </IconButton>
+                          <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                              vertical: 'top',
+                              horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                              vertical: 'top',
+                              horizontal: 'right',
+                            }}
+                            open={Boolean(anchorEl)}
+                            onClose={handleMenuClose}
+                          >
                           <MenuItem>Profile</MenuItem>
-                          <MenuItem>My account</MenuItem>
+                          <MenuItem onClick={handleLogout}><LogoutIcon/> &nbsp; Logout</MenuItem>
                         </Menu>
                       </div>
+
+                      : <Button color="inherit" onClick={handleLogin}>Login</Button>
                     }
                 </Toolbar>
             </AppBar>
